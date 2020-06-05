e96de7a3a53a961720fcc80f1a3354ba894147f5
Jesus Eduardo <lalo.codein22@gmail.com>
2020-06-05 16:57:45 -0500

    Defino metodos like,unlike,liked y likes_count en el Trait Likes

--- app/Traits/Likes.php
+++ app/Traits/Likes.php
@@ -10,4 +10,26 @@ trait Likes
     {
         return $this->morphMany(Like::class, 'likeable');
     }
+
+    public function like()
+    {
+        $this->likes()->firstOrCreate([
+            "user_id" => auth()->id()
+        ]);
+    }
+
+    public function unlike()
+    {
+        $this->likes()->where('user_id', auth()->id())->delete();
+    }
+
+    public function liked()
+    {
+        return $this->likes()->where('user_id',auth()->id())->exists();
+    }
+
+    public function likes_count()
+    {
+        return $this->likes->count();
+    }
 }
