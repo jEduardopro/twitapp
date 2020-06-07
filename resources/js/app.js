c9322a8278ce4fb89a6a90b07a45bc0f599d6e9a
Jesus Eduardo <lalo.codein22@gmail.com>
2020-06-07 14:27:43 -0500

    Creo la tienda para centralizar el estado de la aplicacion

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ require('./bootstrap');
 window.Vue = require('vue');
 Vue.component('login-register-component', require('./components/Auth/LoginRegisterComponent.vue').default);
 
+// Store vuex
+import store from './store/index';
+
 const app = new Vue({
     el: '#app',
+    store
 });
