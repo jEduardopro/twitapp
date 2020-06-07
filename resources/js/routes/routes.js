bd66afa2d3440153cbffe907c1b59625fd3a2826
Jesus Eduardo <lalo.codein22@gmail.com>
2020-06-07 14:44:00 -0500

    Agrego la ruta y vista Home

--- resources/js/routes/routes.js
+++ resources/js/routes/routes.js
@@ -1,9 +1,11 @@
 
 // Views
+import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 
 const routes = [
-    {path: '/login', component: Login, name: 'login'}
+    {path: '', component: Home, name: 'home'},
+    {path: '/login', component: Login, name: 'login'},
 ];
 
 export default routes;
